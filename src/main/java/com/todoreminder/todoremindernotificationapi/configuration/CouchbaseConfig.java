6eeee99631849ec977062fd16537119415f3bd5c
Laser Ayyıldız <laserayyildiz@gmail.com>
2022-09-24 14:52:03 +0300

    add couchbase configs to project

---
+++ src/main/java/com/todoreminder/todoremindernotificationapi/configuration/CouchbaseConfig.java
@@ -0,0 +1,38 @@
+package com.todoreminder.todoremindernotificationapi.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+
+@Configuration
+public class CouchbaseConfig extends AbstractCouchbaseConfiguration {
+
+    @Value("${couchbase.connection-string}")
+    private String connectionString;
+    @Value("${couchbase.userName}")
+    private String userName;
+    @Value("${couchbase.password}")
+    private String password;
+    @Value("${couchbase.bucket-name}")
+    private String bucketName;
+
+    @Override
+    public String getConnectionString() {
+        return connectionString;
+    }
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getBucketName() {
+        return bucketName;
+    }
+}
--- src/main/java/com/todoreminder/todoremindernotificationapi/model/User.java
+++ src/main/java/com/todoreminder/todoremindernotificationapi/model/User.java
@@ -1,32 +1,62 @@
 package com.todoreminder.todoremindernotificationapi.model;
 
 import com.todoreminder.todoremindernotificationapi.dto.response.UserDto;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.couchbase.core.mapping.Document;
+import org.springframework.data.couchbase.core.mapping.Field;
+import org.springframework.data.couchbase.core.mapping.id.GeneratedValue;
+import org.springframework.data.couchbase.core.mapping.id.IdAttribute;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.Set;
+
+import static org.springframework.data.couchbase.core.mapping.id.GenerationStrategy.USE_ATTRIBUTES;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = USE_ATTRIBUTES)
+    private String id;
+
+    @Field
     private String name;
 
-    @Email
+    @Field
     private String email;
 
-    @Size(min = 3)
+    @Field
+    @IdAttribute
     private String username;
 
+    @Field
     private String githubUsername;
 
+    @Field
     private Set<Todo> todos;
 
+    @Field
     private SlackCredentials slackCredentials;
+
+    @Field
+    @LastModifiedDate
+    private Date lastModification;
+
+    @Field
+    @CreatedDate
+    private Date creationDate;
 
     public UserDto toDto() {
         return UserDto.builder().name(name).email(email).username(username).build();
